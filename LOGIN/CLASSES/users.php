0f13f7e0df55464123a555b44ff956635676d1e4
Lucas de Souza <33586128+lucas-de-souza29@users.noreply.github.com>
2021-12-12 23:28:48 -0300

    Update users.php

--- LOGIN/CLASSES/users.php
+++ LOGIN/CLASSES/users.php
@@ -2,10 +2,11 @@
 
 Class Usuario{
 	
-	private $pdo;
-    public $msgErro = "";
+    	private $pdo;
+    	public $msgErro = "";
 
-	public function conectarBD($nome, $host, $usuario, $senha){
+	public function conectarBD($nome, $host, $usuario, $senha)
+	{
 
 		global $pdo;
 		try {
@@ -19,60 +20,58 @@ Class Usuario{
 
 	public function cadastrar($nome, $telefone, $email, $senha){
        
-       global $pdo;
-       //Verificar se o email já existe
-       $sql = $pdo->prepare("SELECT id_user FROM users WHERE email = :e");
-       $sql->bindValue(":e",$email);
-       $sql->execute();
-       if($sql->rowCount() > 0){
+        	global $pdo;
+       	 	//Verificar se o email já existe
+        	$sql = $pdo->prepare("SELECT id_user FROM users WHERE email = :e");
+        	$sql->bindValue(":e",$email);
+        	$sql->execute();
+        	if($sql->rowCount() > 0){
 
-       	//Email já está cadastrado!
+       	  	  //Email já está cadastrado!
        	
-       	  return false;
+       	  	  return false;
 
-       }else{
-       	     //Email ainda não está cadastrado, então irá cadastrar
+        	}else{
+       	     	     //Email ainda não está cadastrado, então irá cadastrar
         
-             $sql = $pdo->prepare("INSERT INTO users (nome,telefone,email,senha) VALUES (:n, :t, :e, :s)");
-             $sql->bindValue(":n", $nome);
-             $sql->bindValue(":t", $telefone);
-             $sql->bindValue(":e", $email);
-             $sql->bindValue(":s", md5($senha));
-             $sql->execute();
+             	     $sql = $pdo->prepare("INSERT INTO users (nome,telefone,email,senha) VALUES (:n, :t, :e, :s)");
+             	     $sql->bindValue(":n", $nome);
+                     $sql->bindValue(":t", $telefone);
+                     $sql->bindValue(":e", $email);
+                     $sql->bindValue(":s", md5($senha));
+                     $sql->execute();
 
-             return true; //Cadastrado com sucesso!
-       }
+                     return true; //Cadastrado com sucesso!
+        	}
 
 	}
 
-    public function logar($email, $senha){
+        public function logar($email, $senha)
+	{
        
-       global $pdo;
-       //Verificar se o email e senha já estão cadastrados no BD
-       $sql = $pdo->prepare("SELECT id_user FROM users WHERE email = :e AND senha = :s");
-       $sql->bindValue(":e",$email);
-       $sql->bindValue(":s",md5($senha));
-       $sql->execute();
-       if($sql->rowCount() > 0){
+       	    global $pdo;
+            //Verificar se o email e senha já estão cadastrados no BD
+            $sql = $pdo->prepare("SELECT id_user FROM users WHERE email = :e AND senha = :s");
+            $sql->bindValue(":e",$email);
+            $sql->bindValue(":s",md5($senha));
+            $sql->execute();
+            if($sql->rowCount() > 0){
 
-       	  //Entrar no sistema (Session)
-       	  $dado = $sql->fetch();
-       	  session_start();
-       	  $_SESSION['id_user'] = $dado['id_user'];
-       	  return true;
+       	      //Entrar no sistema (Session)
+       	      $dado = $sql->fetch();
+       	      session_start();
+       	      $_SESSION['id_user'] = $dado['id_user'];
+       	      
+	      return true;
+		    
+       	     //Logado com sucesso!
+            }else{
 
-       	  //Logado com sucesso!
-
-       }else{
-
-       	//Não foi possível logar!
-       	return false;
-       }
-    }
+       	         //Não foi possível logar!
+       	         return false;
+            }
+        }
 
 }
-
-
-
 
 ?>
